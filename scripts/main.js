a7c388127790105d5b25873a0929839d8acf4d0a
Durgesh <durgeshnidadavolu@gmail.com>
2026-02-09 15:53:18 +0800

    feat: added the comment theme dark light states form setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,27 @@
-// Button that toggles the theme
+// ------>> Global selector references
 
+// Button that toggles the theme
 const themeToggle = document.querySelector('[data-theme-toggle]');
+// Search form element
+const searchForm = document.querySelector('[data-search-form]');
+// Data empty state
+const emptyState = document.querySelector('[data-empty-state]');
+
+// Data Search Input
+const searchInput = document.querySelector('[data-search-input]');
+
+
+// ----->> Helpers
+
+// Data Search Error Message
+
+const displaySearchError = (msg = "") => {
+   const searchErrorElement = document.querySelector('[data-search-error]');
+   if(!searchErrorElement) {
+       return;
+   }
+   searchErrorElement.textContent = msg;
+}
 
 // Toggle between light and dark themes
 
@@ -17,6 +38,18 @@ const themeChanger = () => {
     }
 }
 
+
 // Run theme change when the button is clicked
 
 themeToggle.addEventListener('click', themeChanger);
+
+const searchUser = (e) => {
+    e.preventDefault();    
+    if(!searchInput.value.trim()) {
+       displaySearchError("Please enter a username.");
+    } else {
+        displaySearchError('Hook the API call here..');
+    }
+}
+
+searchForm.addEventListener('submit', searchUser);
